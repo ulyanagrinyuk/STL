208ef75bdc2a17a6518b8073556bb2800e7616c2
Uliana.29 <Uliana.29@DESKTOP-6DD5K9D>
2023-09-11 17:30:41 +0300

    Encoding

--- STL/main.cpp
+++ STL/main.cpp
@@ -45,6 +45,8 @@ void main()
 	//vec.resize(18);
 	vec.reserve(120);
 	vec.shrink_to_fit();
+	vec.insert(vec.begin() + 8, vec.begin() + 3, vec.begin() + 7);
+	vec.insert(vec.begin() + 5, { 1024, 2048, 3072, 4096 });
 	for (int i = 0; i < vec.size(); i++)
 	{
 		cout << vec.at(i) << tab;
@@ -55,12 +57,34 @@ void main()
 	vector_properties(vec2);
 	vec2.push_back(1024);
 	vector_properties(vec2);*/	
-	cout << delimiter << endl;
+	/*cout << delimiter << endl;
 
 	std::vector<double>d_vec = { 2.7, 3.14, 8.3 };
 	vector_properties(d_vec);
+	cout << d_vec.back() << endl;*/
+
+	int index;
+	int count;
+	int value;
+	cout << "¬ведите индекс добавл€емого элемента: "; cin >> index;
+	cout << "¬ведите количество добавл€емымх значений: "; cin >> count;
+	cout << "¬ведите значение добавл€емого значени€: "; cin >> value;
+	vec.insert(vec.begin() + index, count, value);
+	for (std::vector<int>::iterator it = vec.begin(); it < vec.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	for (std::vector<int>::reverse_iterator it = vec.rbegin(); it != vec.rend(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+	for (int i : vec)cout << i << tab; cout << endl;
 #endif // STL_VECTOR
 }
+
+
 template<typename T> void vector_properties(const std::vector<T>& vec)
 {
 	cout << "Size: " << vec.size() << endl;
